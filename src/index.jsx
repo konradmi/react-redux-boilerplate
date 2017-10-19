6211d2102394b4021642ed43c4822dcc05e9ef24
KonradM <miekus.konrad@gmail.com>
2017-10-19 23:30:07 +0200

    Update devDependencies and get rid of history

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
-import createHistory from 'history/createBrowserHistory'
 import reduxThunk from 'redux-thunk'
 
 import reducers from './reducers'
 import App from './components/App'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
-const history = createHistory()
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter history={history}>
+    <BrowserRouter>
       <Route path='/app' component={App}/>
     </BrowserRouter>
   </Provider>
-  ,document.getElementById('root'))
+  ,document.getElementById('root')
+)
